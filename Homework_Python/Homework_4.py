47ce755cee9a13ae8c41e585c6ef1dab1c1f87c1
Anastasia Buglakova <bugla2005@mail.ru>
2022-12-29 12:40:07 +0300

    Checked

--- Homework_Python/Homework_4.py
+++ Homework_Python/Homework_4.py
@@ -12,7 +12,6 @@ def create_and_print_polynominal(ke:int):
     poli_str =''
     for i in range(ke+1):
         dict_1[i]=random.randint(-100,100)
-        print('{}:{}'.format(i,dict_1[i]))
     for k, v in dict_1.items():
         if k==0:
             if v>0:
@@ -41,7 +40,9 @@ def create_and_print_polynominal(ke:int):
                 poli_str = f'{v}*x**{k}{poli_str}'
         else:
             poli_str = f'{v}*x**{k}{poli_str}'
+    print(f'Получившиеся коэффициенты: {dict_1}')
     return poli_str + "=0"
+    
 # print(create_and_print_polynominal(k_number))
 
 # data = open('poli_fail.txt', 'w+')
@@ -54,17 +55,18 @@ def create_and_print_polynominal(ke:int):
 # Расширить значение коэффициентов до [-100..100]
 # гененинуем размер случайных полиномов
 k1_number =random.randint(1,5)
-k2_number=random.randint(1,5)
+k2_number =random.randint(1,5)
 
 if k1_number >k2_number:
     k_max = k1_number 
 else: k_max= k2_number
-print(k1_number, k2_number)
+print(f'Случайно выбранные размеры полиномов:{k1_number}, {k2_number}')
+
 # создаем файл с полиномами
 data = open('poli_fail1.txt', 'w')
 with open('poli_fail1.txt','w') as data:
     data.write(create_and_print_polynominal(k1_number))
-print('\n')
+
 with open('poli_fail2.txt','w') as data:
     data.write(create_and_print_polynominal(k2_number))
 
@@ -77,46 +79,48 @@ content2=''
 with open('poli_fail2.txt','r') as data:
     content2=data.readline()
 
-
-
 def find_sum_in_files(equation:str):
     new_koef = {}
     nq = equation.replace(' ','').replace('=0','').replace('+',' ').replace('-',' -').split(' ')
-    print(nq)
     for item in nq:
         if 'x**' in item:
             a,b = item.split('x**')
-            new_koef[int(b)]=int(item.split('*x')[0])
+            if a =='':
+                new_koef[int(b)]=0
+            elif a =='-':
+                new_koef[int(b)]=-1
+            else:
+                new_koef[int(b)]=int(item.split('*x')[0])
         elif '*x' in item:
-            new_koef[1]=int(item.split('*x')[0]) 
+            new_koef[1]=int(item.split('*x')[0])
+        elif item =='':
+            continue
         else:
             new_koef[0]=item
+    print(f'В виде списка части уравнения выглядят так: {nq}')
     return new_koef
 dict_content1 = find_sum_in_files(content1)
 dict_content2 = find_sum_in_files(content2)
-for item in dict_content1:
-    print('{}:{}'.format(item,dict_content1[item]))
-for item in dict_content2:
-    print('{}:{}'.format(item,dict_content2[item]))
+print(f'Уравнение в форме словаря: \n {dict_content1}')
+print(f'Уравнение в форме словаря: \n {dict_content2}')
+
 poli_sum={}
 # складываем многочлены
-print(f'размер многочлена будет {k_max}')
+print(f'Pазмер суммарного многочлена будет {k_max}')
 for i in range(k_max+1):
     poli_sum[i]=int(dict_content1.get(i, 0))+int(dict_content2.get(i,0))
-for item in poli_sum:
-    print('{}:{}'.format(item,poli_sum[item]))
+print(f'Полином, получившийся путем сложения: {poli_sum}')
 # создаем строку для записи в суммарный файл
 string_sum = ''
 def create_polynominal(poli_sum:dict):
     for k, v in poli_sum.items():
+        if v==0: continue
         if k==0:
             if v>0:
                 string_sum = f'+{v}'
             elif v<0:
                 string_sum = v
-            else:
-                continue
-        elif k == 1 and v > 0:
+        elif k == 1 and v > 0 and k!=k_max:
             string_sum = f'+{v}*x{string_sum}' 
         elif k==1 and v<0:
             string_sum = f'{v}*x{string_sum}'
@@ -132,7 +136,7 @@ def create_polynominal(poli_sum:dict):
         elif k==k_max:
             if v==1 :
                 string_sum = f'x**{k}{string_sum}'
-            if v==-1:
+            elif v==-1:
                 string_sum = f'-x**{k}{string_sum}'
             else:
                 string_sum = f'{v}*x**{k}{string_sum}'
@@ -140,6 +144,7 @@ def create_polynominal(poli_sum:dict):
             string_sum = f'{v}*x**{k}{string_sum}'
     return string_sum + "=0"
 string_result = create_polynominal(poli_sum)
-print(f'String result {string_result}')
+print(f'В файл будем записывать полином в виде строки: {string_result}')
 with open('sum_fail.txt','w') as data:
     data.write(string_result)
+
